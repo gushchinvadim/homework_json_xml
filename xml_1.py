307a80cd87032c1a33e3939a7eb23ab307bab27a
Vadim Gushchin <gushchinvadim@gmail.com>
2024-12-23 17:27:39 +0300

    second commit

--- xml_1.py
+++ xml_1.py
@@ -1,14 +1,20 @@
+from collections import Counter 
+import xml.etree.ElementTree as ET
 def read_xml(file_path, word_max_len=6, top_words_amt=10):
-    import xml.etree.ElementTree as ET
     parser = ET.XMLParser(encoding= 'utf-8')
     tree = ET.parse("newsafr.xml", parser)
     root = tree.getroot()
-    news_list = root.findall("channel/item/description")
-    description_words = []
-    for news in news_list:
-        result = news.text()
-    description_words.append(result)
-    return description_words
+    file_path = root.findall("channel/item/description")
+    popular = []
+
+    for word in file_path:
+          x = (word['description'].split(' '))
+          name = [i for i in x if len(i) > word_max_len]
+          popular.extend(name)
+          words_counter = Counter(popular)
+          top_10 = [w[0] for w in words_counter.most_common(top_words_amt)]
+
+    return top_10
 
 
 if __name__ == '__main__':
