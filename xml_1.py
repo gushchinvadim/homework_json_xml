f45fd4d7e603cf57f4635e5d41b86b71fbc15580
gushchinvadim <gushchinvadim@gmail.com>
2024-12-23 23:40:44 +0300

    third commit

--- xml_1.py
+++ xml_1.py
@@ -1,22 +1,18 @@
-from collections import Counter 
 import xml.etree.ElementTree as ET
-def read_xml(file_path, word_max_len=6, top_words_amt=10):
-    parser = ET.XMLParser(encoding= 'utf-8')
+from collections import Counter
+def read_xml(file, len_word=6, top_words=10):
+    parser = ET.XMLParser(encoding="utf-8")
     tree = ET.parse("newsafr.xml", parser)
     root = tree.getroot()
-    file_path = root.findall("channel/item/description")
+    new_list = root.findall("channel/item")
+    description = new_list[0].find("description")
     popular = []
-
-    for word in file_path:
-          x = (word['description'].split(' '))
-          name = [i for i in x if len(i) > word_max_len]
-          popular.extend(name)
-          words_counter = Counter(popular)
-          top_10 = [w[0] for w in words_counter.most_common(top_words_amt)]
-
+    for row in new_list:    
+        x = row.find("description").text.split(' ')
+        name = [i for i in x if len(i) > len_word]
+        popular.extend(name)
+        words_counter = Counter(popular)
+        top_10 = [w[0] for w in words_counter.most_common(top_words)]
     return top_10
-
-
 if __name__ == '__main__':
     print(read_xml('newsafr.xml'))
-
